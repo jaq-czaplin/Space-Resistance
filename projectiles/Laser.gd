fc0856805eaa202697eb5e636f8ec20c1e1cce17
Jacek Czapliński-Czaja <jacekczaplin@gmail.com>
2024-07-21 14:43:19 +0200

    Added scale effect to lasers

--- projectiles/Laser.gd
+++ projectiles/Laser.gd
@@ -1,8 +1,10 @@
 class_name Laser extends Node2D
 
 @onready var visible_on_screen_notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier
+@onready var scale_sprite_component: ScaleSpriteComponent = $ScaleSpriteComponent
 
 func _ready() -> void:
+	scale_sprite_component.tween_scale()
 	visible_on_screen_notifier.screen_exited.connect(destroy)
 
 func destroy() -> void:
