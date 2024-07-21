6d561f64bf51f586cbf5cf7de692d3b13640992b
Jacek Czapliński-Czaja <jacekczaplin@gmail.com>
2024-07-21 12:43:30 +0200

    Improved movement with MoveStatsResource

---
+++ components/MoveStatsResource.gd
@@ -0,0 +1,4 @@
+class_name MoveStatsResource
+extends Resource
+
+@export var speed: int = 100
--- components/PlayerInputComponent.gd
+++ components/PlayerInputComponent.gd
@@ -2,6 +2,7 @@ class_name PlayerInputComponent
 extends Node
 
 @export var move_component: MoveComponent
+@export var move_stats: MoveStatsResource
 
 func _ready() -> void:
 	# TODO: Auto find move component if not set?
@@ -9,4 +10,4 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	var move_axies = Input.get_axis("move_left", "move_right")
-	move_component.velocity = Vector2(move_axies * 50, 0)
+	move_component.velocity = Vector2(move_axies * move_stats.speed, 0)
