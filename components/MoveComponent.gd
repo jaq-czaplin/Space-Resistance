3cab85f8db50be4a6ba5a2293a4a84659ebe42ba
Jacek Czapliński-Czaja <jacekczaplin@gmail.com>
2024-07-21 12:24:16 +0200

    Created MoveComponent

--- components/MoveComponent.gd
+++ components/MoveComponent.gd
@@ -1,1 +1,11 @@
 class_name MoveComponent extends Node
+
+@export var actor: Node2D
+@export var velocity: Vector2
+
+func _ready():
+	# if actor is not set use parent node
+	if not actor: actor = get_parent()
+
+func _process(delta: float) -> void:
+	actor.translate(velocity * delta)
